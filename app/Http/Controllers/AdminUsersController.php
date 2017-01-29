d53dadb77d5424ae41057f9aa4acb62aafd27a41
Izzat Jamili <izzat@basicinception.com>
2017-01-29 09:24:20 +0800

    Users page working without photos

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UsersRequest;
 use App\Role;
 use Illuminate\Http\Request;
 
@@ -41,10 +42,12 @@ class AdminUsersController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(UsersRequest $request)
     {
         //
-        return $request->all();
+//        return $request->all();
+        User::create($request->all());
+        return redirect('admin/users');
     }
 
     /**
--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -25,7 +25,7 @@
             <th scope="row">{{$user->id}}</th>
             <td>{{$user->name}}</td>
             <td>{{$user->email}}</td>
-            <td>{{$user->role->name}}</td>
+            <td>{{$user->role ? $user->role->name : 'User has no role'}}</td>
             <td>{{$user->is_active == 1 ? 'Active' : 'Not Active '}}</td>
             <td>{{$user->created_at->diffForHumans()}}</td>
             <td>{{$user->updated_at->diffForHumans()}}</td>
